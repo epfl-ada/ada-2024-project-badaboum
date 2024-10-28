e27b9d4a8e7c2e0b84a4d0bea17336a513926468
Yoann Lafore <yoann.lafore@gmail.com>
2024-10-28 22:13:16 +0100

    feat(oscar_scrapper): add argument parser

--- src/script/oscar_scrapper/oscar_scrapper.py
+++ src/script/oscar_scrapper/oscar_scrapper.py
@@ -1,9 +1,42 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from argparse import ArgumentParser
+
 
 def main():
-  pass
+    parser = ArgumentParser(description="Scrape Oscar data")
 
-if __name__ == '__main__':
-  main()
+    parser.add_argument(
+        "base_url",
+        default="https://www.oscars.org/oscars/ceremonies/",
+        type=str,
+        help="Base URL to the Oscar ceremony winners page",
+    )
+
+    parser.add_argument(
+        "years_interval",
+        default=None,
+        type=tuple,
+        help="Years interval to scrape",
+    )
+
+    parser.add_argument(
+        "oscar_categories",
+        default=None,
+        type=list,
+        help="Oscar categories to scrape",
+    ),
+
+    parser.add_argument(
+        "output_file",
+        default="oscar_winners.csv",
+        type=str,
+        help="Output file to save the scraped data",
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
