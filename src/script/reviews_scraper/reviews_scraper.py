28f816763e1aec2561bca4e0da1126962b433715
Yoann Lafore <yoann.lafore@gmail.com>
2024-11-07 21:57:37 +0100

    feat(ratings): generalize scrapping to dataset

--- src/script/reviews_scraper/reviews_scraper.py
+++ src/script/reviews_scraper/reviews_scraper.py
@@ -4,11 +4,12 @@ from argparse import ArgumentParser
 import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from tqdm import tqdm
 import time
+from datetime import datetime
+import os
 
 logging.basicConfig(level=logging.INFO)
 
@@ -17,31 +18,24 @@ def parse_args():
     parser = ArgumentParser(description="Scrape IMDb Reviews")
 
     parser.add_argument(
-        "--base_url",
-        default="https://www.imdb.com/title/tt15398776/reviews/?ref_=tt_ov_urv&spoilers=EXCLUDE&sort=submission_date%2Casc",
-        type=str,
-        help="Base URL to the IMDb reviews page",
-    )
-
-    parser.add_argument(
-        "--output_file",
-        default="imdb_reviews.csv",
+        "--output_directory",
+        default="imdb_reviews/",
         type=str,
         help="Output file to save the scraped reviews",
     )
 
     parser.add_argument(
-        "--stop_year_from_release",
+        "--number_years_from_release",
         default=None,
         type=int,
-        help="Stop scraping reviews after this number of years from the release date, if None, all reviews are scraped",
+        help="Stop loading reviews after this number of years from the release date (reviews beyond this date might still be scrapped), if None, all reviews are loaded and scraped",
     )
 
     parser.add_argument(
-        "--input_dataset_name",
+        "--input_dataset_path",
         required=True,
         type=str,
-        help="Name of the input dataset",
+        help="Path to the input dataset",
     )
 
     parser.add_argument(
@@ -53,27 +47,22 @@ def parse_args():
 
     parser.add_argument(
         "--release_year_column",
-        default="release_date",
+        default="release_year",
         type=str,
         help="Name of the column containing the release year",
     )
 
-    parser.add_argument(
-        "--wait_time_to_scroll_down",
-        default=2,
-        type=int,
-        help="Time to when scrolling down the page (in seconds)",
-    )
-
-    parser.add_argument(
-        "--wait_time_between_reviews_load",
-        default=2,
-        type=int,
-        help="Time to wait between reviews load (in seconds)",
-    )
-
     args = parser.parse_args()
     return args
+
+
+def get_url(imdb_id: str) -> str:
+    BASE_URL = "https://www.imdb.com/title/"
+    ASC_SORT_NO_SPOILERS = (
+        "/reviews/?ref_=tt_ov_urv&spoilers=EXCLUDE&sort=submission_date%2Casc"
+    )
+
+    return BASE_URL + imdb_id + ASC_SORT_NO_SPOILERS
 
 
 def parse_review(review_soup: BeautifulSoup):
@@ -87,19 +76,33 @@ def parse_review(review_soup: BeautifulSoup):
     review_title = review_title_tag.text if review_title_tag else None
 
     review_date_raw = review_soup.find("li", class_="review-date")
-    review_date = review_date_raw.get_text(strip=True) if review_date_raw else None
+    review_date_str = review_date_raw.get_text(strip=True) if review_date_raw else None
+    review_date = (
+        datetime.strptime(review_date_str, "%b %d, %Y") if review_date_str else None
+    )
 
     return review_title, review_text, rating, review_date
 
 
-def scrape_reviews(page_source: str, max_reviews: int):
+def get_reviews_soup_list(page_source: str) -> list:
     soup = BeautifulSoup(page_source, "html.parser")
-    review_soups = soup.find_all("article", class_="user-review-item")
+    return soup.find_all("article", class_="user-review-item")
+
+
+def get_most_recent_review_year(page_source: str) -> int:
+    review_soups = get_reviews_soup_list(page_source)
+
+    # Reviews are scrapped in ascending order of date
+    # Thus, the most recent review is the last one
+    _, _, _, most_recent_review_date = parse_review(review_soups[-1])
+    return most_recent_review_date.year
+
+
+def scrape_reviews(page_source: str):
+    review_soups = get_reviews_soup_list(page_source)
 
     reviews_data = []
     for review_soup in review_soups:
-        if len(reviews_data) >= max_reviews:
-            break
         review_title, review_text, rating, review_date = parse_review(review_soup)
         reviews_data.append(
             {
@@ -126,45 +129,70 @@ def create_driver() -> webdriver.Firefox:
     return driver
 
 
+def load_all_reviews(
+    driver: webdriver.Firefox,
+    max_year: int = None,
+    time_between_click_trials: int = 1,
+    time_between_review_loading: int = 2,
+    max_click_trials: int = 3,
+    timeout: int = 10,
+):
+    span_container = WebDriverWait(driver, timeout).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "chained-see-more-button"))
+    )
+
+    # Find the button within this span
+    see_more_button = span_container.find_element(By.CLASS_NAME, "ipc-see-more__button")
+
+    # Scroll to the button using JavaScript
+    driver.execute_script("arguments[0].scrollIntoView(true);", see_more_button)
+
+    # Try to click the button
+    click_trials = 0
+    click_success = False
+    while not click_success:
+        try:
+            time.sleep(time_between_click_trials)
+            see_more_button.click()
+            click_success = True
+        except Exception as e:
+            if click_trials < max_click_trials:
+                click_trials += 1
+            else:
+                raise
+
+    # Wait for all reviews to be loaded
+    old_nb_reviews = -1
+    current_nb_reviews = get_nb_loaded_reviews(driver.page_source)
+
+    most_recent_review_year = get_most_recent_review_year(driver.page_source)
+
+    # Load reviews until the number of loaded reviews does not change
+    # or until the most recent review is older than the stop year
+    while current_nb_reviews != old_nb_reviews and (
+        max_year == None or most_recent_review_year <= max_year
+    ):
+        old_nb_reviews = current_nb_reviews
+        time.sleep(time_between_review_loading)
+        current_nb_reviews = get_nb_loaded_reviews(driver.page_source)
+        most_recent_review_year = get_most_recent_review_year(driver.page_source)
+        print(f"Reviews loaded for current movie: {current_nb_reviews}", end="\r")
+
+
 def scrape_all_reviews(
-    driver: webdriver.Firefox, base_url: str, stop_year: int = None
+    driver: webdriver.Firefox, base_url: str, max_year: int = None
 ) -> list:
     driver.get(base_url)
 
-    # Perform a click on the button to see all reviews
+    # Load all reviews
     try:
-        # Locate the span with class "chained-see-more-button"
-        span_container = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "chained-see-more-button"))
-        )
-
-        # Find the button within this span
-        see_more_button = span_container.find_element(
-            By.CLASS_NAME, "ipc-see-more__button"
-        )
-
-        # Scroll to the button using JavaScript
-        driver.execute_script("arguments[0].scrollIntoView(true);", see_more_button)
-
-        time.sleep(4)
-
-        see_more_button.click()
-
-        # Wait for all reviews to be loaded
-        old_nb_reviews = -1
-        current_nb_reviews = get_nb_loaded_reviews(driver.page_source)
-
-        while current_nb_reviews != old_nb_reviews:
-            old_nb_reviews = current_nb_reviews
-            time.sleep(2)
-            current_nb_reviews = get_nb_loaded_reviews(driver.page_source)
-            logging.info(f"Loaded {current_nb_reviews} reviews so far...")
-
+        load_all_reviews(driver, max_year=max_year)
     except Exception as e:
-        print(f"An error occurred while trying to click the button: {e}")
+        logging.error(f"Error loading reviews for {base_url}")
+        raise
 
-    reviews = scrape_reviews(driver.page_source, max_reviews=1000)
-    logging.info(f"Scraped {len(reviews)} reviews so far...")
+    # Scrape reviews
+    reviews = scrape_reviews(driver.page_source)
 
     return reviews
 
@@ -173,21 +201,42 @@ def main():
     args = parse_args()
     driver = create_driver()
 
-    try:
-        # Get the reviews data and scrape reviews
-        # reviews_data = get_page_source(driver, args.base_url, args.max_reviews)
-        reviews_data = scrape_all_reviews(driver, args.base_url)
+    # Load the dataset
+    dataset = pd.read_csv(args.input_dataset_path)
+
+    # Create the output directory
+    os.makedirs(args.output_directory, exist_ok=True)
+
+    # Scraping reviews for each movie
+    for _, row in tqdm(dataset.iterrows(), total=dataset.shape[0]):
+        imdb_id = row[args.imdb_id_column]
+        release_year = row[args.release_year_column]
+
+        # Get the URL
+        url = get_url(imdb_id)
+
+        # Scrape the reviews
+        try:
+            reviews_data = scrape_all_reviews(
+                driver, url, max_year=release_year + args.number_years_from_release
+            )
+        except Exception as e:
+            logging.error(f"Error scraping reviews for {imdb_id}")
+            continue
 
         # Create a DataFrame
         reviews_df = pd.DataFrame(reviews_data)
 
+        # Add the IMDb ID
+        reviews_df["imdb_id"] = imdb_id
+
         # Save the data
-        reviews_df.to_csv(args.output_file, index=False)
+        reviews_df.to_csv(f"{args.output_directory}/{imdb_id}_reviews.csv", index=False)
         logging.info(
-            f"Scraped {len(reviews_data)} reviews. Data saved to {args.output_file}"
+            f"Scraped {len(reviews_data)} reviews for {imdb_id}. Data saved to {imdb_id}_reviews.csv"
         )
-    finally:
-        driver.quit()
+
+    driver.quit()
 
 
 if __name__ == "__main__":
