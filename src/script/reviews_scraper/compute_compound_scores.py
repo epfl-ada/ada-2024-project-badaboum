b7ea84bd0ace5ab9bb9550ed11152874dd3c4b34
TheTexanCodeur <92913266+TheTexanCodeur@users.noreply.github.com>
2024-11-11 18:14:35 +0100

    feat: add a script that computes the compound scores of the reviews

---
+++ src/script/reviews_scraper/compute_compound_scores.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+# Get the reviews
+PATH = '../../../data/reviews/'
+reviews = pd.read_csv(PATH + 'imdb_reviews_best_picture_2years_from_release.csv')
+
+# Analyze the compound score of the reviews
+analyzer = SentimentIntensityAnalyzer()
+reviews['text_compound'] = reviews['text'].apply(lambda x : analyzer.polarity_scores(x).get('compound'))
+
+# Store the compound scores
+reviews.to_csv(PATH + 'reviews_with_compound.csv')
