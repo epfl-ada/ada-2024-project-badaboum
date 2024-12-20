a0947aaf1cccead8fc6c7493014a2de9ceb3a538
MatyaAydin <119845483+MatyaAydin@users.noreply.github.com>
2024-12-20 18:44:39 +0100

    renamed question3 folder, added exlanation for actor score and top director

---
+++ src/models/actor_director_bias/__init__.py
@@ -0,0 +1,4 @@
+from src.models.actor_director_bias.utils import *
+from src.models.actor_director_bias.bias_director import *
+from src.models.actor_director_bias.bias_actor import *
+from src.models.actor_director_bias.bias_nomination import *
--- src/models/question3/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.models.question3.utils import *
-from src.models.question3.bias_director import *
-from src.models.question3.bias_actor import *
-from src.models.question3.bias_nomination import *
